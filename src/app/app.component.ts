894f46c13362beebe73d5ceff46a8127d5f651d8
neilnikkunilesh <nileshgupta1993@gmail.com>
2021-04-15 00:04:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { FormGroup, FormControl, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-forms-testing';
+
+  discountRule = new FormGroup({
+    discountPercentage: new FormControl('', []),
+    discountAmount: new FormControl('', [])
+  }, { validators: discountValidator });
+
+  onSubmit() {
+    console.warn(this.discountRule.value);
+  }
+}
+
+// export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+//   return (control: AbstractControl): {[key: string]: any} | null => {
+//     const disPer = control.get('discountPercentage').value;
+//     const disAmt = control.get('discountAmount').value;
+//     if (disPer && disAmt){
+//       return { 'discountError': true }
+//     }
+//     else{
+//       return null;
+//     }
+//   };
+// }
+
+export const discountValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const disPer = control.get('discountPercentage').value;
+  const disAmt = control.get('discountAmount').value;
+  console.log('disAmt: ' + disPer + ' disAmt: ' + disAmt);
+  if (disPer && disAmt) {
+    return { 'discountError': true };
+  } else if (disAmt == null && disPer == null) {
+    return { 'discountError': true }
+  } else if (disAmt === '' && disPer === '') {
+
+  }
+  else {
+    return null;
+  }
 }
